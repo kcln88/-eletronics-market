2d1d63a8d563c28686d78a1c7422584738caa1fe
Keny Ng <kcln88@hotmail.com>
2024-02-28 17:09:40 -0300

    criei o strong params e o set

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :set_order
+
+
+  private
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def orders_params
+    params.require(:order).permit(:quantity, :date)
+  end
 end
