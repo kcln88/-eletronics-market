692f9ccb1f17c83a895729513fd222e659a4638c
Rebeca Yun <rebecajhyun@gmail.com>
2024-02-29 16:12:21 -0300

    criei os cards html e css

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  
-  get "products/new", to: "products#new"
-  post "products", to: "products#create"
-  get "products/edit", to: "products#edit", as: :edit_product
-  patch "products", to: "products#update"
-  get "products/:id", to: "products#show"
 
+  # get "products/new", to: "products#new"
+  # post "products", to: "products#create"
+  # get "products/edit", to: "products#edit", as: :edit_product
+  # patch "products", to: "products#update"
+  # get "products/:id", to: "products#show"
+  resources :products
 
   get "order/new", to: "orders#create"
 
