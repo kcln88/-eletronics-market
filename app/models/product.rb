603269fdd8b333be8d151a9f3bb5052bf91d71ed
Keny Ng <kcln88@hotmail.com>
2024-03-01 19:41:03 -0300

    feito a instalacao do cloudinary

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :orders
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
