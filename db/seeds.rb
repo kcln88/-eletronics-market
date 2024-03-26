992d9f5632e6d961e80bc6d815e92aa0f370244c
jairo goncalves <jairocg@hotmail.com>
2024-03-26 14:51:26 -0300

    ultimas mudanças

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,3 +81,19 @@ user3.save!
   product16.photos.attach(io: file16, filename: "product16.png", content_type: "image/png")
   product16.save!
   p " Seed Finished!"
+
+
+
+
+
+
+  p "starting the Seed!"
+
+emergency1 = Emergency.new({ n_people: 1, category: 1, description: 'Paciente mulher acidentada ao lado da calçada', street: 'Rua jerico 193', neighborhood: 'Vila madalena', city: 'São Paulo' })
+emergency1.save!
+emergency2 = Emergency.new({ n_people: 2, category: 2, description: 'Dois pacientes homens feridos em um acidente de carro', street: 'Rua apinajes 200', neighborhood: 'Perdizes', city: 'São Paulo' })
+emergency2.save!
+emergency3 = Emergency.new({ n_people: 2, category: 3, description: 'Paciente mulher acidentada ao lado da calçada', street: 'avenida paulista 1300', neighborhood: 'bela vista', city: 'São Paulo' })
+emergency3.save!
+
+p "finalizando o seed"
